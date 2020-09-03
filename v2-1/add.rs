30f754e1ae5512810d6f61d2a078628db80805af
Ezzeri Esa <ezzeri.esa@gmail.com>
2020-09-03 12:48:21 -0700

    change v2-1 hello to add

---
+++ v2-1/add.rs
@@ -0,0 +1,8 @@
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+fn main() {
+    println!("{}", add(1, 2));
+}
--- v2-1/hello.rs
+++
@@ -1,8 +0,0 @@
-
-fn hello() -> String {
-    String::from("Hello, world!")
-}
-
-fn main() {
-    println!("{}", hello());
-}
